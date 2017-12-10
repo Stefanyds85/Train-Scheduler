f2c3f8d80eb0215c35a567c4d351f4d4eeb57727
Stefanyds85 <beautyxyzu4@aol.com>
2017-12-10 18:12:17 -0500

    no schedule

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    
+        var config = {
+            apiKey: "AIzaSyDxQqkGa3AKrcGmGVFalJe40g4hdzADf6w",
+            authDomain: "coder-bay-views.firebaseapp.com",
+            databaseURL: "https://coder-bay-views.firebaseio.com",
+            storageBucket: "coder-bay-views.appspot.com",
+            messagingSenderId: "17945436261"
+          };
+          
+          firebase.initializeApp(config);
